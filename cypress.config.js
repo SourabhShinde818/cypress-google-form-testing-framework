88aa0c76ece860f80e38ccf481f1c9c5ea611689
SourabhShinde818 <shindesourabh818@gmail.com>
2024-05-28 21:55:46 +0530

    Google form automation

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,21 @@
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter:"cypress-mochawesome-reporter",
+    
     e2e:{
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
+        require('cypress-mochawesome-reporter/plugin')(on);
 
     },
-    specPattern: 'cypress/integration/googleFormSubmission.cy.js'
+     specPattern: 'cypress/integration/googleFormSubmission_Test.cy.js'
   },
   env:{
-     URL:'https://docs.google.com/forms/d/e/1FAIpQLScPfEbpaoUu3WVwSDM9wIFX5uo1XQ1xpuHNtP7cF_rkR-o8Zg/viewform'
+     URL:'https://docs.google.com/forms/d/e/1FAIpQLScPfEbpaoUu3WVwSDM9wIFX5uo1XQ1xpuHNtP7cF_rkR-o8Zg/viewform',
+
   }
   
 
