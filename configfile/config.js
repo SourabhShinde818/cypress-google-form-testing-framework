68e99d346a758d5b26c9a40bfc5ae78a64d8f86d
SourabhShinde818 <shindesourabh818@gmail.com>
2024-05-29 22:58:07 +0530

    google from test

---
+++ configfile/config.js
@@ -0,0 +1,16 @@
+
+const locators = {
+
+    googleForm: {
+
+        NameInput:"input[jsname='YPqjbf']",
+        AgeRadioButton:"#i12 > .vd3tt > .AB7Lab",
+        ExerciseFrequencyDropdown:".KKjvXb > .vRMGwf",
+        SelectExerciseoption:".OA0qNb > [data-value='Yes'] > .vRMGwf",
+        submitButton:".lRwqcd > .uArJ5e > .l4V7wb > .NPEfkd",
+        successMessage : ".vHW8K"
+        
+    }
+    
+    };
+    module.exports = { locators };
--- cypress/pageObjects/googleFormPage.js
+++ cypress/pageObjects/googleFormPage.js
@@ -1,41 +1,31 @@
+import { locators } from "../../configfile/config"
 
 
 export class googleFormPage{
 
-    constructor(){
-             this.NameInput="input[jsname='YPqjbf']"
-             this.AgeRadioButton="#i12 > .vd3tt > .AB7Lab"
-             this.ExerciseFrequencyDropdown=".KKjvXb > .vRMGwf"
-             this.SelectExerciseoption=".OA0qNb > [data-value='Yes'] > .vRMGwf"
-             this.submitButton =".lRwqcd > .uArJ5e > .l4V7wb > .NPEfkd"
-             this.successMessage = ".vHW8K"
-            }
-
-    
-   
     openURl(){
         cy.visit(Cypress.env('URL')+"/forms/d/e/1FAIpQLScPfEbpaoUu3WVwSDM9wIFX5uo1XQ1xpuHNtP7cF_rkR-o8Zg/viewform")
     }
     enterName(Name){
-        cy.get(this.NameInput).type(Name)
+        cy.get(locators.googleForm.NameInput).type(Name)
     }
     selectAge() {
         
-        cy.get(this.AgeRadioButton).click()
+        cy.get(locators.googleForm.AgeRadioButton).click()
 
     }
     selectExerciseFrequency(){
-        cy.get(this.ExerciseFrequencyDropdown).click()
+        cy.get(locators.googleForm.ExerciseFrequencyDropdown).click()
         cy.wait(2000)
-        cy.get(this.SelectExerciseoption).click()
+        cy.get(locators.googleForm.SelectExerciseoption).click()
     }
     submitForm() {
-        cy.get(this.submitButton).click()
+        cy.get(locators.googleForm.submitButton).click()
     }
     
 
     verifySubmissionSuccess() {
-       return cy.get(this.successMessage)
+       return cy.get(locators.googleForm.successMessage)
 
         }
     verifyURLchange(){
