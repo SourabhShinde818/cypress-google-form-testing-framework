db0bbf941d5d2968511bc64c0def890f8f5df17d
Sourabh Shinde <90339465+SourabhShinde818@users.noreply.github.com>
2024-05-29 23:32:01 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,25 +2,30 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
   reporter:"cypress-mochawesome-reporter",
     defaultCommandTimeout: 6000,
     
     "retries":{
-      "runMode":1,
+      "runMode":1, 
     },
 
     e2e:{
     
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
         require('cypress-mochawesome-reporter/plugin')(on);
 
     },
+      
      specPattern: 'cypress/integration/googleFormSubmission_Test.cy.js'
+      
   },
+  
   env:{
+    
      URL:'https://docs.google.com',
-
+    
   }
   
 });
