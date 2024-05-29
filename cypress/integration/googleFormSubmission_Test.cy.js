983c3b4bc5a818d25e40f15a678f0860f3ef77e6
Sourabh Shinde <90339465+SourabhShinde818@users.noreply.github.com>
2024-05-29 23:29:55 +0530

    Update googleFormSubmission_Test.cy.js

--- cypress/integration/googleFormSubmission_Test.cy.js
+++ cypress/integration/googleFormSubmission_Test.cy.js
@@ -1,20 +1,21 @@
-/// <reference types="cypress" /> "../../"
 import { googleFormPage } from "../pageObjects/googleFormPage"
 
 const formobj = new googleFormPage();
 
-
-
 var formData;
 
 describe('test automation',function(){
+  
   before(function(){
+    
     formobj.openURl()
     cy.fixture('googleFormTestData').then(function(testdata){
       formData=testdata
+      
     })
       
   })
+  
   it('Google Form Submission Test',function(){
     
      formobj.enterName(formData.name)
@@ -24,7 +25,6 @@ describe('test automation',function(){
      formobj.verifySubmissionSuccess().should('have.text',formData.validationMessage)
      //form response is added in url
      formobj.verifyURLchange().should('include',formData.changedUrl)
-
 
   })
 
