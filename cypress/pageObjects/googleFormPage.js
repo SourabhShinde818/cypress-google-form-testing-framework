1203d2c07689eea3dbecdcd5d953fc32d3e68654
Sourabh Shinde <90339465+SourabhShinde818@users.noreply.github.com>
2024-05-29 23:28:19 +0530

    Update googleFormPage.js

--- cypress/pageObjects/googleFormPage.js
+++ cypress/pageObjects/googleFormPage.js
@@ -4,34 +4,47 @@ import { locators } from "../../configfile/config"
 export class googleFormPage{
 
     openURl(){
+        
         cy.visit(Cypress.env('URL')+"/forms/d/e/1FAIpQLScPfEbpaoUu3WVwSDM9wIFX5uo1XQ1xpuHNtP7cF_rkR-o8Zg/viewform")
+        
     }
+    
     enterName(Name){
         cy.get(locators.googleForm.NameInput).type(Name)
+        
     }
+    
     selectAge() {
         
         cy.get(locators.googleForm.AgeRadioButton).click()
 
     }
+    
     selectExerciseFrequency(){
         cy.get(locators.googleForm.ExerciseFrequencyDropdown).click()
         cy.wait(2000)
         cy.get(locators.googleForm.SelectExerciseoption).click()
-    }
-    submitForm() {
-        cy.get(locators.googleForm.submitButton).click()
+        
     }
     
-
+    submitForm() {
+        
+        cy.get(locators.googleForm.submitButton).click()
+        
+    }
+    
     verifySubmissionSuccess() {
+        
        return cy.get(locators.googleForm.successMessage)
 
-        }
+   }
+    
     verifyURLchange(){
+        
         return cy.url()
     }
-    }
+    
+ }
 
 
 
